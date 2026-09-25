5855ac2036f94397a9e5fd17972a93251b6f99c0
jordanmesser04 <jordan.messer04@gmail.com>
2026-09-25 10:41:08 +1000

    Created main python file. Added code to read the csv files.

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Read the csv files into dataframes
+HarrisPartI = pd.read_csv('dataset/HarrisPartI.csv')
+HarrisPartIII = pd.read_csv('dataset/HarrisPartIII.csv')
+Krause21 = pd.read_csv('dataset/Krause21.csv')
+vandenBerg_table2 = pd.read_csv('dataset/vandenBerg_table2.csv')
+#print(HarrisPartI.head())
+#print(HarrisPartI.info())
+
+
