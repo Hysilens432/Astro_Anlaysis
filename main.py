38f397d7c8670899afa7afab1067d7c327969bdf
Helektra <169999458+Hysilens432@users.noreply.github.com>
2026-09-25 20:20:18 +1000

    update main.py to reflect standard practices

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+# Constants 
+HARRIS_I = "./dataset/HarrisPartI.csv"
+HARRIS_III = "./dataset/HarrisPartIII.csv"
+KRAUSE_21 = "./dataset/Krause21.csv"
+VANDENBERG_TABLE2 = "./dataset/vandenBerg_table2.csv"
+
 import pandas as pd
 
 # Read the csv files into dataframes
-HarrisPartI = pd.read_csv('dataset/HarrisPartI.csv')
-HarrisPartIII = pd.read_csv('dataset/HarrisPartIII.csv')
-Krause21 = pd.read_csv('dataset/Krause21.csv')
-vandenBerg_table2 = pd.read_csv('dataset/vandenBerg_table2.csv')
+HarrisPartI = pd.read_csv(HARRIS_I)
+HarrisPartIII = pd.read_csv(HARRIS_III)
+Krause21 = pd.read_csv(KRAUSE_21)
+vandenBerg_table2 = pd.read_csv(VANDENBERG_TABLE2)
 #print(HarrisPartI.head())
 #print(HarrisPartI.info())
 
