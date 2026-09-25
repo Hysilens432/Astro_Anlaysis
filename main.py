32759cb0316c52f7548981bbdc18c2c3bdd82dbd
Helektra <169999458+Hysilens432@users.noreply.github.com>
2026-09-25 20:24:12 +1000

    update main.py, simplify imports, add comments

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+# Main file for implimenting data analysis 
+# on given datasets
+
+# Import necessary libraries
+import pandas as pd
+from utils.csv_import import csv_import
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 # Constants 
 HARRIS_I = "./dataset/HarrisPartI.csv"
 HARRIS_III = "./dataset/HarrisPartIII.csv"
 KRAUSE_21 = "./dataset/Krause21.csv"
 VANDENBERG_TABLE2 = "./dataset/vandenBerg_table2.csv"
 
-import pandas as pd
-
 # Read the csv files into dataframes
-HarrisPartI = pd.read_csv(HARRIS_I)
-HarrisPartIII = pd.read_csv(HARRIS_III)
-Krause21 = pd.read_csv(KRAUSE_21)
-vandenBerg_table2 = pd.read_csv(VANDENBERG_TABLE2)
+HarrisPartI = csv_import(HARRIS_I)
+HarrisPartIII = csv_import(HARRIS_III)
+Krause21 = csv_import(KRAUSE_21)
+vandenBerg_table2 = csv_import(VANDENBERG_TABLE2)
 #print(HarrisPartI.head())
 #print(HarrisPartI.info())
 
