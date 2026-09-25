3e987e7d4d02223a405082e8c1c05f81af7df477
Helektra <169999458+Hysilens432@users.noreply.github.com>
2026-09-25 20:18:31 +1000

    program csv_import.py utility function

--- utils/csv_import.py
+++ utils/csv_import.py
@@ -1,6 +1,15 @@
-def csv_import():
+import os 
+import pandas as pd
+
+def csv_import(path):
     '''
     Perform Path Validation,
     Import CSV file as dataframes, return the
-    data frame object
+    data frame object corresponding to the CSV file at the given path.
     '''
+    if not path.endswith('.csv'):
+        raise ValueError("The provided path does not point to a CSV dataset")
+    elif not os.path.exists(path):
+        raise ValueError("The file path does not exist")
+    return pd.read_csv(path)
+ 
